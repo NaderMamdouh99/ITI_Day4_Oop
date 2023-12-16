51d145129fede64cc1d06673f58979b3272ff94e
NaderMamdouh99 <Nader.mamdouh9898@gmail.com>
2023-12-16 16:15:55 +0200

    Add project files.

---
+++ Complex.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnswerDay6
+{
+    internal class Complex
+    {
+        private int _real;
+        private int _imaginary;
+
+        public Complex(int real, int imagaginary)
+        {
+            _real = real;
+            _imaginary = imagaginary;
+        }
+        public Complex():this(1,1)
+        {
+            
+        }
+        //public Complex(int real, int imagaginary) : this(real, imagaginary)
+        //{
+
+
+        //}
+
+        #region Property
+
+
+        public int Real
+        {
+            get { return _real; }
+            set { _real = value; }
+        }
+
+        public int Imaginary
+        {
+            get { return _imaginary; }
+            set { _imaginary = value; }
+        }
+
+        #endregion
+
+        #region Method
+        public double GetMagnitude()
+        {
+            
+           return Math.Sqrt(Math.BigMul(Math.Abs(_real), Math.Abs(_imaginary)));
+        }
+
+        public static bool operator >=  (Complex c1,Complex c2)
+        {
+            return c1.Real >= c2.Real && c1.Imaginary >= c2.Imaginary;
+        }
+        public static bool operator  <=  (Complex c1,Complex c2)
+        {
+            return c1.Real <= c2.Real && c1.Imaginary <= c2.Imaginary;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+             
+            return new Complex(c1._real + c2._real, c1.Imaginary + c2.Imaginary);
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+             
+            return new Complex(c1._real - c2._real, c1.Imaginary - c2.Imaginary);
+        }
+        public static Complex operator ++(Complex c1)
+        {
+             
+            return new Complex(c1._real + 1, c1.Imaginary+1);
+        }
+        public static Complex operator --(Complex c1)
+        {
+             
+            return new Complex(c1._real -1, c1.Imaginary-1);
+        }
+        public override string ToString()
+        {
+            return  ""+_real +"+"+Imaginary+"i";
+        }
+
+        //casting 
+        
+        public static implicit operator Complex(int value)
+        {
+            
+            return new Complex(value,200);
+        }
+
+        public static explicit operator int(Complex c)
+        {
+            return c.Real;
+        }
+
+        //equals
+        public override bool Equals(object obj)
+        {
+            Complex c = (Complex)obj;
+             return c._real == this._real && c._imaginary == this._imaginary;
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnswerDay6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Lab
+            Complex c1 = new Complex(7, 6);
+
+            Console.WriteLine(c1.GetMagnitude());
+            Complex c2 = new Complex(4, 7);
+            Complex c3 = new Complex(4, 7);
+            //adding
+            Console.WriteLine(c1 + c2);
+            //minus
+            Console.WriteLine(c1 - c2);
+            //++
+            Console.WriteLine(c1++);
+            //--
+            Console.WriteLine(c1--);
+
+            //casting
+            c1 = (Complex)10;
+            Console.WriteLine("cast: " + c1);
+
+            int value;
+            value = (int)c1;
+            Console.WriteLine("cast: " + value);
+
+            Console.WriteLine("equal :: " + c1.Equals(c2));
+            Console.WriteLine("equal :: " + c2.Equals(c3)); 
+            #endregion
+
+        }
+    }
+}
